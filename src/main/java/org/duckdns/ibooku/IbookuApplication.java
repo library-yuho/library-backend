1128bc88f834c5adc1db1e991e8e2d1dba57682c
rigizer <rigizer@gmail.com>
2024-08-20 22:53:40 +0900

    [ADD] init project

---
+++ src/main/java/org/duckdns/ibooku/IbookuApplication.java
@@ -0,0 +1,13 @@
+package org.duckdns.ibooku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbookuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbookuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/duckdns/ibooku/IbookuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.ibooku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbookuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
