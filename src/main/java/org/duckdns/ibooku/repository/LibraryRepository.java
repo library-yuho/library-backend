0a3cc87ab6774d4c046b90fee08886bc961c1f01
rigizer <rigizer@gmail.com>
2024-08-26 00:29:33 +0900

    [ADD] 10km(반경 5km) 이내 도서관 목록 조회, 현재 위치로부터 도서관까지의 거리 추가

--- src/main/java/org/duckdns/ibooku/repository/LibraryRepository.java
+++ src/main/java/org/duckdns/ibooku/repository/LibraryRepository.java
@@ -2,7 +2,12 @@ package org.duckdns.ibooku.repository;
 
 import org.duckdns.ibooku.entity.library.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LibraryRepository extends JpaRepository<Library, Integer> {
-
+    @Query(value = "SELECT * FROM library l WHERE l.lat BETWEEN :lat - 0.05 AND :lat + 0.05 AND l.lon BETWEEN :lon - 0.05 AND :lon + 0.05", nativeQuery = true)
+    List<Library> findByLatLonRange(@Param("lat") double lat, @Param("lon") double lon);
 }
--- src/main/java/org/duckdns/ibooku/service/library/LibraryService.java
+++ src/main/java/org/duckdns/ibooku/service/library/LibraryService.java
@@ -30,22 +30,108 @@ public class LibraryService {
     private final LibraryRepository libraryRepository;
 
     public List<LibraryResponseDTO> list(String isbn, double lat, double lon) {
+        return libraryRepository.findByLatLonRange(lat, lon).stream()
+                .map(library -> {
+                    boolean isBookExist = isBookExist(isbn, library.getLibCode());
+                    double distance = distance(lat, lon, library);
 
-
-        return libraryRepository.findAll().stream()
-                .map(library -> LibraryResponseDTO.builder()
-                        .id(library.getId())
-                        .name(library.getName())
-                        .address(library.getAddress())
-                        .content(library.getContent())
-                        .telephone(library.getTelephone())
-                        .website(library.getWebsite())
-                        .lat(library.getLat())
-                        .lon(library.getLon())
-                        .isBookExist(false)
-                        .distance(1.23)
-                        .build())
+                    return LibraryResponseDTO.builder()
+                            .id(library.getId())
+                            .name(library.getName())
+                            .address(library.getAddress())
+                            .content(library.getContent())
+                            .telephone(library.getTelephone())
+                            .website(library.getWebsite())
+                            .lat(library.getLat())
+                            .lon(library.getLon())
+                            .isBookExist(isBookExist)
+                            .distance(distance)
+                            .build();
+                    }
+                )
                 .collect(Collectors.toList());
+    }
+
+    private boolean isBookExist(String isbn, String libCode) {
+//        String url = String.format("https://data4library.kr/api/libSrch?authKey=%s&pageNo=%d&pageSize=10&format=json", KEY_LIBRARY_BIGDATA, page);
+//
+//        Header header = new Header();
+////                .append("User-Agent", HTTPUtils.USER_AGENT)
+////                .append("Accept-Language", HTTPUtils.ACCEPT_LANGUAGE)
+////                .append("Accept-Encoding", HTTPUtils.ACCEPT_ENCODING)
+////                .append("Connection", HTTPUtils.CONNECTION);
+//
+//        try {
+//            Get get = new Get(url)
+//                    .setHeader(header)
+//                    .execute();
+//
+//            int responseCode = get.getResponseCode();
+//            if (responseCode != org.apache.http.HttpStatus.SC_OK) {
+//                log.debug("responseCode: {}", responseCode);
+//                throw new RuntimeException("통신 오류: " + get.getUrl());
+//            }
+//
+//            JsonObject jsonObject = JSONUtils.parse(get.getResult());
+//            JsonObject response = jsonObject.getAsJsonObject("response");
+//            JsonArray libs = response.getAsJsonArray("libs");
+//
+//
+//            for (Object o: libs) {
+//                JsonObject obj = (JsonObject) o;
+//                JsonObject lib = obj.getAsJsonObject("lib");
+//
+//                String name = lib.get("libName").getAsString();
+//                String libCode = lib.get("libCode").getAsString();
+//                String address = lib.get("address").getAsString();
+//                String content = lib.get("operatingTime") == null ? null : lib.get("operatingTime").getAsString();
+//                String telephone = lib.get("tel").getAsString();
+//                String website = lib.get("homepage").getAsString();
+//                Double lat = Double.parseDouble(lib.get("latitude").getAsString());
+//                Double lon = Double.parseDouble(lib.get("longitude").getAsString());
+//
+//                Library library = Library.builder()
+//                        .name(name)
+//                        .libCode(libCode)
+//                        .address(address)
+//                        .content(content)
+//                        .telephone(telephone)
+//                        .website(website)
+//                        .lat(lat)
+//                        .lon(lon)
+//                        .build();
+//
+//                libraryRepository.save(library);
+//
+//                System.out.println(num + "/1458 -> 남은개수 : " + (1458-num));
+//                num++;
+//            }
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//            log.error("통신 오류가 발생했습니다.");
+//            throw new RuntimeException();
+//        }
+
+        return false;
+    }
+
+    private double distance(double lat, double lon, Library library) {
+        double theta = lon - library.getLon();
+        double dist = Math.sin(deg2rad(lat)) * Math.sin(deg2rad(library.getLat())) + Math.cos(deg2rad(lat)) * Math.cos(deg2rad(library.getLat())) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        return dist * 1609.344;
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
     }
 
     public String insert() {
@@ -107,7 +193,7 @@ public class LibraryService {
                 }
             } catch(Exception e) {
                 e.printStackTrace();
-                log.error("날씨 조회 중 통신 오류가 발생했습니다.");
+                log.error("통신 오류가 발생했습니다.");
                 throw new RuntimeException();
             }
         }
