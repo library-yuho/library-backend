3effa0d806e794354c30b3d2bfac36b511e8090f
rigizer <rigizer@gmail.com>
2024-08-26 17:25:52 +0900

    [MOD] 도서 ISBN기준 정보 조회 시 국립중앙도서관 API를 사용하여 조회하도록 변경

--- src/main/java/org/duckdns/ibooku/service/book/BookService.java
+++ src/main/java/org/duckdns/ibooku/service/book/BookService.java
@@ -17,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -27,7 +26,6 @@ public class BookService {
     @Value("${api-key.nl}")
     private String KEY_NL;
 
-    private final BookRepository bookRepository;
     private final ReviewRepository reviewRepository;
 
     public List<BookResponseDTO> search(String keyword) {
@@ -85,24 +83,56 @@ public class BookService {
         }
     }
 
-    public BookResponseDTO info(String isbn) {
-//        Book book = bookRepository.findByIsbn(isbn);
-        double point = 0.0;
+    public BookResponseDTO info(String searchIsbn) {
+        String url = String.format("https://www.nl.go.kr/NL/search/openApi/search.do?key=%s&systemType=%s&pageNum=1&pageSize=30&apiType=json&detailSearch=true&isbnOp=isbn&isbnCode=%s", KEY_NL, "오프라인자료", searchIsbn);
 
+        Header header = new Header();
 
+        try {
+            Get get = new Get(url)
+                    .setHeader(header)
+                    .execute();
 
+            int responseCode = get.getResponseCode();
+            if (responseCode != org.apache.http.HttpStatus.SC_OK) {
+                log.debug("responseCode: {}", responseCode);
+                throw new RuntimeException("통신 오류: " + get.getUrl());
+            }
 
-        return BookResponseDTO.builder().build();
+            JsonObject jsonObject = JSONUtils.parse(get.getResult());
+            JsonArray result = jsonObject.getAsJsonArray("result");
 
-//        return BookResponseDTO.builder()
-//                .name(book.getName())
-//                .isbn(book.getIsbn())
-//                .image()
-//                .subject()
-//                .author(book.getAuthor())
-//                .publisher(book.getPublisher())
-//                .content(book.getContent())
-//                .point(point)
-//                .build();
+            for (Object o: result) {
+                JsonObject book = (JsonObject) o;
+
+                String name = book.get("titleInfo").getAsString();
+
+                String[] isbnArray = book.get("isbn").getAsString().split(" ");
+                String isbn = isbnArray.length == 0 ? "-" : isbnArray[0];
+                String image = book.get("imageUrl").getAsString();
+                String subject = book.get("kdcName1s").getAsString();
+                String author = book.get("authorInfo").getAsString();
+                String publisher = book.get("pubInfo").getAsString();
+                String content = "";
+                Double point = 0.0;
+
+                return BookResponseDTO.builder()
+                        .name(name)
+                        .isbn(isbn)
+                        .image(image)
+                        .subject(subject)
+                        .author(author)
+                        .publisher(publisher)
+                        .content(content)
+                        .point(point)
+                        .build();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            log.error("통신 오류가 발생했습니다.");
+            throw new RuntimeException();
+        }
+
+        return null;
     }
 }
